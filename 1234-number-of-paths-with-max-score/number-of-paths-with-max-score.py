9090118842e0c26aecef8c795a2eb08f636264c4
Ayush Jyala <ayushjtech@gmail.com>
2026-07-05 21:36:58 +0530

    Time: 108 ms (100.00%) | Memory: 12.8 MB (100.00%) - LeetSync

---
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def pathsWithMaxScore(self, board):
+        """
+        :type board: List[str]
+        :rtype: List[int]
+        """
+        n = len(board)
+        MOD = 10**9 + 7
+
+        score = [[-1] * n for _ in range(n)]
+        ways = [[0] * n for _ in range(n)]
+
+        score[n - 1][n - 1] = 0
+        ways[n - 1][n - 1] = 1
+
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+
+                if board[i][j] == 'X':
+                    continue
+
+                if i == n - 1 and j == n - 1:
+                    continue
+
+                best = -1
+                cnt = 0
+
+                for x, y in [(i + 1, j), (i, j + 1), (i + 1, j + 1)]:
+                    if x >= n or y >= n:
+                        continue
+                    if score[x][y] == -1:
+                        continue
+
+                    if score[x][y] > best:
+                        best = score[x][y]
+                        cnt = ways[x][y]
+                    elif score[x][y] == best:
+                        cnt = (cnt + ways[x][y]) % MOD
+
+                if best == -1:
+                    continue
+
+                if board[i][j] == 'E':
+                    val = 0
+                else:
+                    val = int(board[i][j])
+
+                score[i][j] = best + val
+                ways[i][j] = cnt
+
+        if ways[0][0] == 0:
+            return [0, 0]
+
+        return [score[0][0], ways[0][0]]
